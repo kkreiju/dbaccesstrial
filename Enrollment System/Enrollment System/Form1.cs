03fcee9c6bcb5155a73655f20346389739cbdc52
kkreiju <138178348+kkreiju@users.noreply.github.com>
2024-04-15 12:08:42 +0800

    Add files via upload

---
+++ Enrollment System/Enrollment System/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Enrollment_System
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            string connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=\\Server2\second semester 2023-2024\LAB802\79286_CC_APPSDEV22_1030_1230_PM_MW\79286-23220726\Desktop\FINAL\Saguisa.accdb";
+            OleDbConnection thisConnection = new OleDbConnection(connectionString);
+            string Ole = "Select * From SUBJECTFILE";
+            OleDbDataAdapter thisAdapter = new OleDbDataAdapter(Ole, thisConnection);
+            OleDbCommandBuilder thisBuilder = new OleDbCommandBuilder(thisAdapter);
+            DataSet thisDataSet = new DataSet();
+            thisAdapter.Fill(thisDataSet, "SubjectFile");
+
+            DataRow thisRow = thisDataSet.Tables["SubjectFile"].NewRow();
+            thisRow["SFSUBJCODE"] = Convert.ToInt16(SubjectCodeTextBox.Text);
+            thisRow["SFSUBJDESC"] = DescriptionTextBox.Text;
+            thisRow["SFSUBJUNITS"] = UnitsTextBox.Text;
+            thisRow["SFSUBJREGOFRNG"] = OfferingComboBox.Text.Substring(0, 1);
+            thisRow["SFSUBJCATEGORY"] = CategoryComboBox.Text.Substring(0, 1);
+            thisRow["SFSUBJSTATUS"] = "AC";
+            thisRow["SFSUBJCOURSECODE"] = CourseCodeComboBox.Text; //textbox ni ha
+            thisRow["SFSUBJCURRCODE"] = CurriculumYearTextBox.Text;
+
+            thisDataSet.Tables["SubjectFile"].Rows.Add(thisRow);
+            thisAdapter.Update(thisDataSet, "SubjectFile");
+
+            MessageBox.Show("Recorded");
+        }
+    }
+}
